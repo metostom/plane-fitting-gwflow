29249a2ed105d743f1ba2d5b374b687584ed8a34
metostom <metostom@gmail.com>
2021-01-16 17:14:10 -0800

    Add Docstrings

--- PlaneFit.py
+++ PlaneFit.py
@@ -5,6 +5,25 @@ from scipy.optimize import leastsq,least_squares
 
 class PlaneFit():
 
+    """
+    PlaneFit Data Class
+
+    Attributes
+    ----------
+    locnamecol : str
+        location name column header
+    xcol : str
+        x-coordinate column header
+    ycol : str
+        y-coordinate column header
+    zcol : str
+        z-coordinate column header
+    tcol : str
+        time stamp column header   
+    clusters : list
+        list of cluster lists to run the plane fitting on
+    """
+
     def __init__(self,filepath,locnamecol, xcol,ycol,zcol,tcol,clusters):
 
         self.filepath = filepath
@@ -18,6 +37,17 @@ class PlaneFit():
 
 
     def planeFit3Point(self,xs,ys,zs):  
+        '''
+        Solves the equation of a plane (Ax + By + Cz - D = 0) that intersects the 3 points
+
+            Parameters:
+                    xs (array) : array of x-coordinates for the points:
+                    ys (array) : array of y-coordinates for the points:
+                    zs (array) : array of z-coordinates for the points:
+                                
+            Returns:
+                    fit (array) : array of the plane coefficients (A,B,C,D)
+        '''
     
         x1,x2,x3 = xs
         y1,y2,y3 = ys
@@ -35,11 +65,25 @@ class PlaneFit():
         return a,b,c,d
 
     def planeFitLeastSquares(self,xs,ys,zs):
+
+        '''
+        Optimizes the equation of a plane (Ax + By + Cz - D = 0) that intersects the given points
+
+            Parameters:
+                    xs (array) : array of x-coordinates for the points:
+                    ys (array) : array of y-coordinates for the points:
+                    zs (array) : array of z-coordinates for the points:
+                                
+            Returns:
+                    fit (array) : array of the plane coefficients (A,B,C,D)
+                    r2 (float) : sum of residuals squared - goodness of fit metric
+        '''
     
         XYZ = np.vstack([xs,ys,zs])
         # Inital guess of the plane
         p0 = [0.005, 0.005, 0.005, 0.05]
 
+        # minimizing function
         def f_min(X,p):
             plane_xyz = p[0:3]
             distance = (plane_xyz*X.T).sum(axis=1) + p[3]
@@ -54,21 +98,39 @@ class PlaneFit():
         se = residual.var()
         sez = np.array(zs).var()
         r2 = 1 - se/sez
-        #r = (f_min(XYZ, sol)**2).sum()
-        # calculate the R2 value
         
         return fit, r2
 
 
     def gradient(self,fit):
+        '''
+        Calculates the gradient of a plane given the coefficients of the plane equation (Ax + By + Cz - D = 0)
+
+            Parameters:
+                    fit (array) : array of the plane coefficients (A,B,C,D)
+                                
+            Returns:
+                    gradient (float) : gradient of the plane
+        '''
         a,b,c,d = fit
-        return float(np.sqrt(((a**2 + b**2)/c**2)))
+        gradient = float(np.sqrt(((a**2 + b**2)/c**2)))
+
+        return gradient
 
     def direction(self,fit):
+        '''
+        Calculates the direction of maximum gradient of a plane given the coefficients of the plane equation (Ax + By + Cz - D = 0) 
+        and converts the angle to compass degrees. 
+
+            Parameters:
+                    fit (array) : array of the plane coefficients (A,B,C,D)
+                                
+            Returns:
+                    direction (float) : compass direction of maximum gradient, in degrees from north
+        '''
         
         a,b,c,d = fit    
         theta = np.arctan(b/a) * 180/np.pi
-
         if a > 0:
             angle = 90 - theta
         elif a < 0:
@@ -82,10 +144,36 @@ class PlaneFit():
                 angle = None
         if c < 0:
             angle = angle + 180
-            
-        return float(np.mod(angle,360))
+        
+        # modulus to 360 degrees
+        direction = float(np.mod(angle,360))
+
+        return direction
     
     def analyzeCluster(self,data,wells,wellCol,timeCol,xCol,yCol,zCol):
+        '''
+        Computes the direction, gradient, R2, and fit for each time the well cluster has data 
+
+            Parameters:
+                    data (dataframe) : df of all x,y,z,t data
+                    wells (list) : cluster to analyze
+                        locnamecol : str
+                    wellCol : str
+                        location name column header
+                    xCol : str
+                        x-coordinate column header
+                    yCol : str
+                        y-coordinate column header
+                    zCol : str
+                        z-coordinate column header
+                    timeCol : str
+                        time stamp column header  
+
+                                
+            Returns:
+                    clusterdf (dataframe) : DataFrame of results for the cluster of the form:
+                        DateTime | Plane Fit Coefficients | Gradient | Direction | R2
+        '''
     
         dfsub = data.loc[data[wellCol].isin(wells)]
         times = pd.unique(dfsub[timeCol])
@@ -123,6 +211,17 @@ class PlaneFit():
         return clusterdf
 
     def analyzeAllClusters(self):
+        '''
+        Analyzes all well clusters
+                                
+            Returns:
+                    Results df (dataframe) : DataFrame of results for each cluster of the form:
+                        Well Cluster | Results DataFrame
+                    
+                    Where Results DataFrame is of the form:
+                    DateTime | Plane Fit Coefficients | Gradient | Direction | R2
+        '''
+
         
         results = []
         for c in self.clusters:
